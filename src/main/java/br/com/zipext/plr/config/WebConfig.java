52e676b81e8a99d57ba1d67c66c860e116426c31
AdailCarvalho <adail.dux@gmail.com>
2019-01-21 10:09:14 -0300

    Atualiza properties / ips frontend

--- src/main/java/br/com/zipext/plr/config/WebConfig.java
+++ src/main/java/br/com/zipext/plr/config/WebConfig.java
@@ -10,16 +10,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 	
-	@Value("${app.frontend.ip}")
-	private String frontEndAddress;
+	@Value("${app.frontend.ext.ip}")
+	private String frontEndAddressExt;
+	
+	@Value("${app.frontend.int.ip}")
+	private String frontEndAddressInt;
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/colaboradores/**")
-			.allowedOrigins(this.frontEndAddress)
+			.allowedOrigins(this.frontEndAddressInt, this.frontEndAddressExt)
 			.allowedMethods("GET","POST","DELETE","PUT","HEAD","OPTIONS");
 		registry.addMapping("/metaEspecifica/**")
-			.allowedOrigins(this.frontEndAddress)
+			.allowedOrigins(this.frontEndAddressInt, this.frontEndAddressExt)
 			.allowedMethods("GET","POST","DELETE","PUT","HEAD","OPTIONS");
 	}
 }
