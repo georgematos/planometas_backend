a090b43ebe645eea62efe006b04b732bcefffdfd
George Prolins <georgeprolins@pop-os.localdomain>
2021-02-10 15:33:45 -0300

    feat: exibicao de denominadores e numeradores para planejado e realizado concluido

--- src/main/java/br/com/zipext/plr/service/FolhaMetaMensalService.java
+++ src/main/java/br/com/zipext/plr/service/FolhaMetaMensalService.java
@@ -18,49 +18,47 @@ public class FolhaMetaMensalService {
 
 	@Autowired
 	private FolhaMetaMensalRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public Long countByMeta(MetasModel meta) {
 		return this.repository.countByMeta(meta);
 	}
-	
+
 	@Modifying
 	@Transactional(readOnly = false)
 	public void deleteAll(List<FolhaMetaMensalModel> models) {
 		this.repository.deleteAll(models);
 	}
-	
+
 	@Transactional(readOnly = false)
 	public void deleteByMeta(MetasModel meta) {
 		this.repository.deleteByMeta(meta);
 	}
-	
+
 	@Transactional(readOnly = false)
 	public void deleteByMetaColaboradorAndAno(MetasModel meta, ColaboradorModel colaborador, Integer ano) {
 		this.repository.deleteByMetaColaboradorAndAno(meta, colaborador, ano);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public List<FolhaMetaMensalModel> findAll() {
-		return
-				this.repository.findAll();
+		return this.repository.findAll();
 	}
-	
+
 	@Transactional(readOnly = true)
-	public List<FolhaMetaMensalModel> findByMetaColaboradorAndAno(MetasModel meta, ColaboradorModel colaborador, Integer ano) {
-		return
-				this.repository.findByMetaColaboradorAndAno(meta, colaborador, ano);
+	public List<FolhaMetaMensalModel> findByMetaColaboradorAndAno(MetasModel meta, ColaboradorModel colaborador,
+			Integer ano) {
+		return this.repository.findByMetaColaboradorAndAno(meta, colaborador, ano);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public List<FolhaMetaMensalModel> findByFolhaMetaItem(FolhaMetaItemModel folhaMetaItem) {
-		List<FolhaMetaMensalModel> models = 
-				this.repository.findByMetaAndAno(folhaMetaItem.getMeta(), folhaMetaItem.getFolhaMeta().getInicioVigencia().getAno());
-		
-		return
-				models;
+		List<FolhaMetaMensalModel> models = this.repository.findByMetaAndAno(folhaMetaItem.getMeta(),
+				folhaMetaItem.getFolhaMeta().getInicioVigencia().getAno());
+
+		return models;
 	}
-	
+
 	@Transactional(readOnly = false)
 	public List<FolhaMetaMensalModel> saveAll(List<FolhaMetaMensalModel> models) {
 		return this.repository.saveAll(models);
