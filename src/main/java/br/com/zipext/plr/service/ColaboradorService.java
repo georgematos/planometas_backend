fc2a70b9041ea6379c131b93dcbf7082a993343b
George Prolins <georgeprolins@pop-os.localdomain>
2021-02-18 19:20:11 -0300

    temp: telas de pesquisa e cadastro colaborador

--- src/main/java/br/com/zipext/plr/service/ColaboradorService.java
+++ src/main/java/br/com/zipext/plr/service/ColaboradorService.java
@@ -2,6 +2,7 @@ package br.com.zipext.plr.service;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,41 +23,49 @@ public class ColaboradorService {
 
 	@Autowired
 	private ColaboradorRepository repository;
-	
+
 	@Autowired
 	private PropertyService propertyService;
-	
+
 	@Autowired
 	private TemplateCampoService templateCampoService;
-	
+
 	public ByteArrayInputStream export() throws IOException {
-		XlsFileExport export = new XlsFileExport(this.propertyService.getProperty(EnumProperty.XLS_TEMPLATE_COLABORADORES_PATH), EnumXLSSheets.COLABORADORES);
+		XlsFileExport export = new XlsFileExport(
+				this.propertyService.getProperty(EnumProperty.XLS_TEMPLATE_COLABORADORES_PATH),
+				EnumXLSSheets.COLABORADORES);
 		byte emptyBuff[] = new byte[] {};
 
 		List<ColaboradorModel> models = this.repository.findAll();
 		if (models != null && !models.isEmpty()) {
-			export.processTable(models, 
+			export.processTable(models,
 					this.templateCampoService.findByTemplateAndArea(
-							new TemplateModel(EnumXLSTemplates.COLABORADORES.getCodigo()), EnumXLSArea.COLABORADORES.getArea()));
-			return
-					export.writeToFile();
+							new TemplateModel(EnumXLSTemplates.COLABORADORES.getCodigo()),
+							EnumXLSArea.COLABORADORES.getArea()));
+			return export.writeToFile();
 		}
-	
+
 		return new ByteArrayInputStream(emptyBuff);
 	}
-	
+
 	@Transactional(readOnly = true)
-	public List<ColaboradorModel> findByFilter(String matricula, String cpf, String nome, String situacao, String cargo, String diretoria, String time) {
-		return 
-				this.repository.findByFilter(matricula, cpf, nome, situacao, cargo, diretoria, time);
+	public List<ColaboradorModel> findAll() {
+		List<ColaboradorModel> colabs = repository.findAll();
+		colabs.sort(Comparator.comparing(ColaboradorModel::getNome));
+		return colabs;
 	}
-	
+
+	@Transactional(readOnly = true)
+	public List<ColaboradorModel> findByFilter(String matricula, String cpf, String nome, String situacao, String cargo,
+			String diretoria, String time, String superiorImediato) {
+		return this.repository.findByFilter(matricula, cpf, nome, situacao, cargo, diretoria, time, superiorImediato);
+	}
+
 	@Transactional(readOnly = true)
 	public ColaboradorModel findByMatricula(String matricula) {
-		return
-				this.repository.findByMatricula(matricula);
+		return this.repository.findByMatricula(matricula);
 	}
-	
+
 	@Transactional(readOnly = false)
 	public ColaboradorModel save(ColaboradorModel model) {
 		return this.repository.save(model);
