7ccc438b838943db18a19541c35133d845bb6f56
George Prolins <george.matos@plolins.com.br>
2021-03-02 17:50:24 -0300

    feat: substituicao da aba metas pela aba consultas

--- src/main/java/br/com/zipext/plr/repository/FolhaMetaItemRepository.java
+++ src/main/java/br/com/zipext/plr/repository/FolhaMetaItemRepository.java
@@ -23,4 +23,45 @@ public interface FolhaMetaItemRepository extends JpaRepository<FolhaMetaItemMode
 	public void deleteByIdFolhaMeta(@Param("idFolhaMeta") Long idFolhaMeta);
 	
 	public List<FolhaMetaItemModel> findByFolhaMetaOrderBySequenciaAsc(FolhaMetaModel folhaMeta);
+	
+//	@Query("select model from FolhaMetaItemModel model "
+////			+  "join model.folhaMeta.colaborador colab "
+//			+  "join model.folhaMeta.inicioVigencia ini "
+//			+  "join model.folhaMeta.fimVigencia fim "
+//			+  "left join model.folhaMeta fm "			
+////			+  "where colab = :colaborador "
+//			+  "where ini.id >= :inicioVigencia "
+//			+  "and fim.id <= :fimVigencia "
+//			+  "and model.meta.id = :metaId "
+//			+  "and fm is not null "
+//			+  "order by model.meta.descricao asc")
+	
+	
+//	@Query("select model from FolhaMetaItemModel model "
+//			+  "join model.folhaMeta fm "
+////			+  "join model.folhaMeta.inicioVigencia ini "
+////			+  "join model.folhaMeta.fimVigencia fim "
+//			+  "on model.meta.id = :metaId "
+//			+  "and fm.inicioVigencia.id >= :inicioVigencia "
+//			+  "and fm.fimVigencia.id <= :fimVigencia "
+//			+  "order by model.meta.descricao asc")
+//	public List<FolhaMetaItemModel> findByMetaAndPeriodo(
+//			@Param("metaId") Long metaId,
+////			@Param("colaborador") ColaboradorModel colababorador,
+//			@Param("inicioVigencia") Long inicioVigencia,
+//			@Param("fimVigencia") Long fimVigencia
+//	);
+	
+	@Query("select model from FolhaMetaItemModel model "			
+	+  "inner join model.folhaMeta fm "
+	+  "where model.meta.id = :metaId "
+	+  "and model.folhaMeta.inicioVigencia.id >= :inicioVigencia "
+	+  "and model.folhaMeta.fimVigencia.id <= :fimVigencia "
+	+  "order by model.meta.descricao asc")
+public List<FolhaMetaItemModel> findByMetaAndPeriodo(
+	@Param("metaId") Long metaId,
+//	@Param("colaborador") ColaboradorModel colababorador,
+	@Param("inicioVigencia") Long inicioVigencia,
+	@Param("fimVigencia") Long fimVigencia
+);
 }
