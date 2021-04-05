e564b9cd0a5a49e7f4b0ab0db6d981e0e30f7e4c
George Prolins <george.matos@plolins.com.br>
2021-04-05 11:30:56 -0300

    style: formatacao automatica do classe MetasController

--- src/main/java/br/com/zipext/plr/service/MetasService.java
+++ src/main/java/br/com/zipext/plr/service/MetasService.java
@@ -33,84 +33,81 @@ public class MetasService {
 
 	@Autowired
 	private MetasRepository repository;
-	
+
 	@Autowired
 	private PropertyService propertyService;
-	
+
 	@Autowired
-	private TemplateCampoService templateCampoService;	
-	
+	private TemplateCampoService templateCampoService;
+
 	public ByteArrayInputStream export(TempoModel periodo) throws IOException {
-		XlsFileExport export = new XlsFileExport(this.propertyService.getProperty(EnumProperty.XLS_TEMPLATE_INDICADORES_PATH), EnumXLSSheets.INDICADORES);
+		XlsFileExport export = new XlsFileExport(
+				this.propertyService.getProperty(EnumProperty.XLS_TEMPLATE_INDICADORES_PATH),
+				EnumXLSSheets.INDICADORES);
 		byte emptyBuff[] = new byte[] {};
 
 		List<MetasModel> models = this.findAll(periodo);
 		if (models != null && !models.isEmpty()) {
-			export.processTable(models, 
-					this.templateCampoService.findByTemplateAndArea(
-							new TemplateModel(EnumXLSTemplates.INDICADORES.getCodigo()), EnumXLSArea.INDICADORES.getArea()));
-			return
-					export.writeToFile();
+			export.processTable(models, this.templateCampoService.findByTemplateAndArea(
+					new TemplateModel(EnumXLSTemplates.INDICADORES.getCodigo()), EnumXLSArea.INDICADORES.getArea()));
+			return export.writeToFile();
 		}
-	
+
 		return new ByteArrayInputStream(emptyBuff);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public List<MetasModel> findAllAtivasByOrderByDescricaoAsc() {
-		return
-				this.repository.findAllAtivasByOrderByDescricaoAsc(EnumSituacao.ATIVO.getCodigo().toString());
+		return this.repository.findAllAtivasByOrderByDescricaoAsc(EnumSituacao.ATIVO.getCodigo().toString());
 	}
-	
+
 	@Transactional(readOnly = true)
 	public List<MetasModel> findAll(TempoModel periodo) {
 		return this.repository.findAll(periodo);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public List<MetasModel> findByDescricaoAndSituacao(String descricao, String situacao) {
-		return
-				this.repository.findByDescricaoAndSituacao(descricao, situacao);
+		return this.repository.findByDescricaoAndSituacao(descricao, situacao);
 	}
-	
+
 	@Transactional(readOnly = true)
-	public List<MetasModel> findByFilter(MetasModel model, String codigoLegado, String meta, String situacao, TipoMedicaoModel tipoMedicao, TipoMetaModel tipoMeta,
-			FormulaModel formula, FrequenciaMedicaoModel frequenciaMedicao) {
-		return 
-				this.repository.findByFilter(model, codigoLegado, meta, situacao, tipoMedicao, tipoMeta, formula, frequenciaMedicao);
+	public List<MetasModel> findByFilter(MetasModel model, String codigoLegado, String meta, String situacao,
+			TipoMedicaoModel tipoMedicao, TipoMetaModel tipoMeta, FormulaModel formula,
+			FrequenciaMedicaoModel frequenciaMedicao) {
+		return this.repository.findByFilter(model, codigoLegado, meta, situacao, tipoMedicao, tipoMeta, formula,
+				frequenciaMedicao);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public MetasModel findById(Long id) {
 		Optional<MetasModel> model = this.repository.findById(id);
 		if (model.isPresent()) {
 			return model.get();
 		}
-		
+
 		return null;
 	}
-	
+
 	@Transactional(readOnly = true)
-	public List<MetasModel> findProjetosVencidosByResponsavel(Integer periodoPLR, Long skyDataLimite, ColaboradorModel aprovador) {
-		return 
-			this.repository.findProjetosVencidosByResponsavel(periodoPLR, skyDataLimite, aprovador, Arrays.asList(EnumTipoMeta.PROJETO.name(), EnumTipoMeta.ENTREGA.name()));
+	public List<MetasModel> findProjetosVencidosByResponsavel(Integer periodoPLR, Long skyDataLimite,
+			ColaboradorModel aprovador) {
+		return this.repository.findProjetosVencidosByResponsavel(periodoPLR, skyDataLimite, aprovador,
+				Arrays.asList(EnumTipoMeta.PROJETO.name(), EnumTipoMeta.ENTREGA.name()));
 	}
 
 	@Transactional(readOnly = true)
 	public List<MetasModel> findQualitativas() {
-		return
-				this.repository.findByQualificadorMeta(EnumQuantQual.QUALITATIVA.getCodigo().toString());
+		return this.repository.findByQualificadorMeta(EnumQuantQual.QUALITATIVA.getCodigo().toString());
 	}
-	
+
 	@Transactional(readOnly = true)
 	public List<MetasModel> findQuantitativas() {
-		return
-				this.repository.findByQualificadorMeta(EnumQuantQual.QUANTITATIVA.getCodigo().toString());
+		return this.repository.findByQualificadorMeta(EnumQuantQual.QUANTITATIVA.getCodigo().toString());
 	}
-	
+
 	@Transactional(readOnly = false)
 	public MetasModel save(MetasModel model) {
 		return this.repository.save(model);
 	}
 }
-	
